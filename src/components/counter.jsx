3a849bc9088517b9f818bbc1f19125cca858d084
Janice-M <janiceink001@gmail.com>
2019-10-31 16:38:58 +0300

    JSX magic

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,9 +3,26 @@ import React, { Component } from 'react';
 
 
 class Counter extends Component {
-    
+    state={
+        count:0
+    };
     render() { 
-        return <h1>Hey Grey</h1>;
+        return (
+
+        <React.Fragment >
+                <h1 class='text-center'>  Grey's Shop</h1>
+                <br/>
+                <br/>
+
+                <span> {this.state.count}</span>
+                <button>Increment</button>
+
+        </React.Fragment>
+        );
+    }
+
+    formatCount (){
+        return this.state.count === 0 ? 'zero':this.state.count;
     }
 }
  
