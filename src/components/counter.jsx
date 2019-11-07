090766dd0a939847cb6252f363b4a66676a12c31
Janice-M <janiceink001@gmail.com>
2019-11-07 15:36:33 +0300

    hii stuff inarun kweli

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,7 +5,6 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state={
         count: 0
-        imageUrl: 'https://images.unsplash.com/photo-1453856908826-6bbeda0f8490?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=933&q=80'
     };
     render() { 
         return (
@@ -14,7 +13,7 @@ class Counter extends Component {
                 <h1 class='text-center'>  Grey's Shop</h1>
                 <br/>
                 <br/>
-                <img src= { this.state.imageUrl } alt=""/>
+                
                 <span> {this.formatCount()}</span>
                 <button>Increment</button>
 
