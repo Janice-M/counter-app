b2ea35fe23227259007282544bcf1da5e88e22c0
Janice-M <janiceink001@gmail.com>
2019-11-12 13:31:33 +0300

    added a new state called tags

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -21,6 +21,14 @@ class Counter extends Component {
                 <span className= {this.getBadgeClasses()}> {this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
 
+                <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+
+
+
+                </ul>
             </div>
         );
     }
