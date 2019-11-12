653dbd847b692fc74b49bb3390a92a02e81b6185
Janice-M <janiceink001@gmail.com>
2019-11-12 13:25:10 +0300

    getBadge class added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,8 +10,7 @@ class Counter extends Component {
     
 
     render() {
-        let classes = "badge m-2 badge-";
-        classes += ( this.state.count === 0)?"warning" : "primary"; 
+        
 
 
         return (
@@ -21,11 +20,17 @@ class Counter extends Component {
                 <br />
                 <br />
 
-                <span className= {classes}> {this.formatCount()}</span>
+                <span className= {this.getBadgeClasses()}> {this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
 
             </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
