b8a49c23f99751eac353928c6e2440260fa5a305
Janice-M <janiceink001@gmail.com>
2020-01-28 21:50:38 +0300

    class name

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -19,7 +19,7 @@ class Counter extends Component {
                 <br />
                 <br />
                 <br />
-                <p>this is a full metal alchemist
+                <p>this is a fullmetal alchemist</p> 
 
                 <span className= {this.getBadgeClasses()}> {this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">increment button</button>
